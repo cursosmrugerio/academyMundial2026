4213e8a9ce1e11ad369b8f8f0e99d06b163e5185
mike <mrugerio@gmail.com>
2026-06-04 15:47:13 -0600

    feat(abstracta): atributos, constante final y miembro static en clase abstracta Animal

---
+++ abstracta/src/com/curso/v0/Animal.java
@@ -0,0 +1,11 @@
+package com.curso.v0;
+
+public abstract class Animal {
+	
+	//HAS-A
+	String tipo;
+	final int jaula = 1; //Constante
+	static StringBuilder zoologico = 
+			new StringBuilder("Chapultec");
+
+}
