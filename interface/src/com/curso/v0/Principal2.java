883f14abb831bc196b8ca8c1ea50d1cd1798b3a9
mike <mrugerio@gmail.com>
2026-06-04 15:47:13 -0600

    feat(interface): herencia múltiple entre interfaces con extends

---
+++ interface/src/com/curso/v0/Principal2.java
@@ -0,0 +1,18 @@
+package com.curso.v0;
+
+class Clase1 {}
+
+interface A {}
+
+interface B extends A {}
+
+interface C extends B {}
+
+interface D {}
+
+interface E extends A,D {}
+
+
+public class Principal2 {
+
+}
