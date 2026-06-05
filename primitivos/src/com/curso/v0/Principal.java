6a1ffbbb482eb9e7e476738ae54db2103e3ba288
mike <mrugerio@gmail.com>
2026-06-05 16:21:13 -0600

    feat(primitivos): tipos primitivos y literales numéricos (v0)
    Literales en decimal, octal, binario y hexadecimal con int.

---
+++ primitivos/src/com/curso/v0/Principal.java
@@ -0,0 +1,23 @@
+package com.curso.v0;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int x = 1; //DECIMAL
+		int y = 010; //OCTAL
+		int z = 0B000101; //BINARIO
+		int w = 0XF; //HEXADECIMAL
+		
+		int String = 0;
+		
+		System.out.println(x+y); //9
+
+		System.out.println(y);
+		
+		System.out.println(z);
+		
+		System.err.println(w);
+	}
+
+}
